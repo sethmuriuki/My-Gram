3899369e182eb4b314b849f586ae8fa344414d49
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-01-26 15:15:54 +0300

    add image model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'photos/', null = True)
+    name = models.CharField(max_length=30)
+    caption = models.TextField()
+    profile = models.ManyToManyField(categories)
+    
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+import datetime as dt
 # Create your views here.
+def timeline(request):
+    post = Images.objects.all()
+    return render(request, 'timeline.html', {'post':post})
