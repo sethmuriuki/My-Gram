6caf3aa1d816c53d570324b0d800394503b33ca9
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2018-02-02 08:19:06 +0300

    update registration

--- posts/views.py
+++ posts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 import datetime as dt
 from .models import Images
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+# @login_required(login_url = '/accounts/login/')
 def timeline(request):
     post = Images.objects.all()
     return render(request, 'timeline.html', {'post': post})
